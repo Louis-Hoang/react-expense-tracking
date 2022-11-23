d485a62a64cf349199acb747e99ce2030dc3067e
hoangxtung <hoang_t1@denison.edu>
2022-11-23 16:00:46 -0500

    finish-PR

---
+++ expense-tracking/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,35 @@
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+import React, { useState } from "react";
+function NewExpense(props) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString(),
+        };
+        props.onAddExpense(expenseData);
+    };
+    const starEditingHandler = () => {
+        setIsEditing(true);
+    };
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    };
+    return (
+        <div className="new-expense">
+            {!isEditing && (
+                <button onClick={starEditingHandler}>Add New Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEditingHandler}
+                />
+            )}
+        </div>
+    );
+}
+
+export default NewExpense;
